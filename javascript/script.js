a05eb597defeb1c29e60673dcebc08cbdb72963e
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-01-05 19:02:35 -0300

    uptade 2.0

---
+++ javascript/script.js
@@ -0,0 +1,67 @@
+let plt = document.getElementById('plt-header');
+let plt_des = document.getElementById('politica');
+
+plt.addEventListener('mouseenter', function(){
+    plt_des.style.display = 'flex'
+})
+
+plt.addEventListener('mouseout', function(){
+    plt_des.style.display = 'none'
+})
+
+let brasil = document.getElementById('plt-brasil');
+let plt_brasil = document.getElementById('brasil');
+
+brasil.addEventListener('mouseenter', function(){
+    plt_brasil.style.display = 'flex'
+})
+
+brasil.addEventListener('mouseout', function(){
+    plt_brasil.style.display = 'none'
+})
+
+let economia = document.getElementById('plt-economia');
+let plt_economia = document.getElementById('economia');
+
+economia.addEventListener('mouseenter', function(){
+    plt_economia.style.display = 'flex'
+})
+
+economia.addEventListener('mouseout', function(){
+    plt_economia.style.display = 'none'
+})
+
+let esporte = document.getElementById('ptl-esporte');
+let plt_esporte = document.getElementById('esporte');
+
+esporte.addEventListener('mouseenter', function(){
+    plt_esporte.style.display = 'flex'
+})
+
+esporte.addEventListener('mouseout', function(){
+    plt_esporte.style.display = 'none'
+})
+
+let entretenimento = document.getElementById('ptl-entretenimento');
+let plt_entretenimento = document.getElementById('entretenimento');
+
+entretenimento.addEventListener('mouseenter', function(){
+    plt_entretenimento.style.display = 'flex'
+})
+
+entretenimento.addEventListener('mouseout', function(){
+    plt_entretenimento.style.display = 'none'
+})
+
+// colunistas 
+
+let colunistas = document.getElementById('ptl-colunistas');
+let plt_colunistas = document.getElementById('colunistas');
+
+colunistas.addEventListener('mouseenter', function(){
+    plt_colunistas.style.display = 'flex'
+})
+
+colunistas.addEventListener('mouseout', function(){
+    plt_colunistas.style.display = 'none'
+})
